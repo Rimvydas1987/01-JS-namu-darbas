3ae816bc0e73db9122e69e2c65afe8b64fdf7821
Rimvydas Šimkus <84323090+Rimvydas1987@users.noreply.github.com>
2021-06-09 09:28:52 +0300

    +Nr.3

--- script.js
+++ script.js
@@ -7,32 +7,11 @@ skaicius1 = 1;
 skaicius2 = 2;
 skaicius3 = 3;
 skaicius4 = 4;
-var suma, sandauga;
-suma = skaicius1 + skaicius2 + skaicius3 + skaicius4;
-sandauga = skaicius1 * skaicius2 * skaicius3 * skaicius4;
 
 //NR.3
-
-//NR.3 (Nr.4)
-
-//NR.4 (Nr.5)
-
-//NR.5 (Nr.6)
-var maxSkaicius;
-maxSkaicius = Number.MAX_VALUE
-
-//Papildoma NR.1
-var skaicius5, skaicius6;
-//Papildoma NR.2
-
-//Papildoma NR.3
-
-
-
-
-
-
-
+var suma, sandauga, tmp;
+suma = skaicius1 + skaicius2 + skaicius3 + skaicius4;
+sandauga = skaicius1 * skaicius2 * skaicius3 * skaicius4;
 
 console.log("skaicius1 = " + skaicius1);
 console.log("skaicius2 = " + skaicius2);
@@ -40,5 +19,45 @@ console.log("skaicius3 = " + skaicius3);
 console.log("skaicius4 = " + skaicius4);
 console.log("suma = " + suma);
 console.log("sandauga = " + sandauga);
+
+
+//NR.3 (Nr.4)
+tmp = skaicius1;
+skaicius1 = skaicius2;
+skaicius2 = tmp;
+
+console.log("skaicius1 = " + skaicius1);
+console.log("skaicius2 = " + skaicius2);
+
+//NR.4 (Nr.5)
+alert("skaicius 3 = " + (skaicius3 = skaicius4) + " skaicius4 = " + (skaicius4 = skaicius3))
+
+
+//NR.5 (Nr.6)
+var maxSkaicius;
+maxSkaicius = Number.MAX_VALUE
+
 console.log("maxSkaicius = " + maxSkaicius);
+
+//Papildoma NR.1
+var skaicius5, skaicius6;
+skaicius5 = 5;
+skaicius6 = 6;
+//Papildoma NR.2
+
+//Papildoma NR.3
+
+
+
+//https://betterexplained.com/articles/swap-two-variables-using-xor/
+
+
+
+
+
+
+
+
+
+
 
