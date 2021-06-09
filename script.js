412035230f893665513b890352a82282401edb4d
Rimvydas Šimkus <84323090+Rimvydas1987@users.noreply.github.com>
2021-06-09 08:48:32 +0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,44 @@
+//NR.1
+"use strict"
+
+//NR.2
+var skaicius1, skaicius2, skaicius3, skaicius4;
+skaicius1 = 1;
+skaicius2 = 2;
+skaicius3 = 3;
+skaicius4 = 4;
+var suma, sandauga;
+suma = skaicius1 + skaicius2 + skaicius3 + skaicius4;
+sandauga = skaicius1 * skaicius2 * skaicius3 * skaicius4;
+
+//NR.3
+
+//NR.3 (Nr.4)
+
+//NR.4 (Nr.5)
+
+//NR.5 (Nr.6)
+var maxSkaicius;
+maxSkaicius = Number.MAX_VALUE
+
+//Papildoma NR.1
+var skaicius5, skaicius6;
+//Papildoma NR.2
+
+//Papildoma NR.3
+
+
+
+
+
+
+
+
+console.log("skaicius1 = " + skaicius1);
+console.log("skaicius2 = " + skaicius2);
+console.log("skaicius3 = " + skaicius3);
+console.log("skaicius4 = " + skaicius4);
+console.log("suma = " + suma);
+console.log("sandauga = " + sandauga);
+console.log("maxSkaicius = " + maxSkaicius);
+
